73960ff8ce2fef1bb9c035891e7e41d9d1acfea5
josefernandotrindade <josefernandotrindade@gmail.com>
2018-09-20 00:23:48 +0100

    adding more configuration entities.

--- src/Controller/SliderController.php
+++ src/Controller/SliderController.php
@@ -19,14 +19,14 @@ class SliderController extends ControllerBase {
 
     $slider_style = \Drupal::entityManager()->getStorage('image_style')->load('slider');
 
-    if ($node->hasField('field_slider')) {
+    if ($node->hasField('field_slider') and !$node->field_slider->isEmpty()) {
       foreach ($node->field_slider as $slide) {
         $entity = $slide->entity;
         $image = $entity->field_slider_image->first()->entity;
 
         $sliders['data'][] = [
-          'title' => Xss::filter($entity->field_title->first()->value),
-          'body' =>  $entity->field_body->isEmpty() ? '' : Xss::filter($entity->field_body->first()->value),
+          'title' => Xss::filter($entity->field_slider_title->first()->value),
+          'body' =>  $entity->field_slider_body->isEmpty() ? '' : Xss::filter($entity->field_slider_body->first()->value),
           'image' => $slider_style->buildUrl($image->getFileUri()),  
         ];
       }
