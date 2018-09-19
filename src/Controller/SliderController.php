5138a031d699aff81c00e198b0c8693773cd0e36
josefernandotrindade <josefernandotrindade@gmail.com>
2018-09-19 23:24:16 +0100

    prepare image field and image style

--- src/Controller/SliderController.php
+++ src/Controller/SliderController.php
@@ -16,13 +16,14 @@ class SliderController extends ControllerBase {
       'data' => [],
     ];
 
+    $slider_style = \Drupal::entityManager()->getStorage('image_style')->load('slider');
+
     if ($node->hasField('field_slider')) {
       foreach ($node->field_slider as $slide) {
-        
         $sliders['data'][] = [
           'title' => $slide->entity->field_title->first()->value,
-          'image' => NUL,
-          'body' => NULL,  
+          'body' => $slide->entity->field_body->first()->value,
+          'image' => $slider_style->buildUrl($slide->entity->field_slider_image->first()->entity->getFileUri()),  
         ];
       }
     }
